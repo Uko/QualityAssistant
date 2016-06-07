d9a47d937366454ac280cb405f3be91069c19f11
Uko <uko@unikernel.net>
2016-06-07 18:38:13 +0200

    Finally good conf for travis

--- .utility/flatDeploy.sh
+++ .utility/flatDeploy.sh
@@ -8,7 +8,8 @@ set -o errexit
 readonly REPO_URL="http://smalltalkhub.com/mc/YuriyTymchuk/ScrapYard/main/"
 readonly PACKAGE_NAME="ConfigurationOfFlatQA"
 
-curl get.pharo.org/vm60 | bash
+#set the correct vm
+SMALLTALK_CI_VM=`find $SMALLTALK_CI_VMS -name pharo -type f -perm +0111 | head -n 1`
 
 # load the conf package
 ./pharo $SMALLTALK_CI_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
