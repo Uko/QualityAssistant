4f2501a0a6c6b80261b346761c3d7e9fad90c82d
Uko <uko@unikernel.net>
2016-06-07 16:41:48 +0200

    Updated the deploy script

--- .utility/flatDeploy.sh
+++ .utility/flatDeploy.sh
@@ -5,12 +5,11 @@ set -o errexit
 
 
 # variables that we use
-SMALLTALK_VM="$(find $SMALLTALK_CI_VMS -name pharo -type f -executable | head -n 1) --nodisplay"
 REPO_URL="http://smalltalkhub.com/mc/YuriyTymchuk/ScrapYard/main/"
 PACKAGE_NAME="ConfigurationOfFlatQA"
 
 # load the conf package
-$SMALLTALK_VM $SMALLTALK_CI_IMAGE eval --save "Gofer new url: $REPO_URL username: $HUB_USER password: $HUB_PASS; package: $PACKAGE_NAME; load."
+$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
 
 # store the version
-$SMALLTALK_VM $SMALLTALK_CI_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
+$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
