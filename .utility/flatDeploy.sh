83caa8bc49f1692da7de6734c6933276b3e2c585
Uko <uko@unikernel.net>
2016-06-07 16:52:09 +0200

    updated the deploy script

--- .utility/flatDeploy.sh
+++ .utility/flatDeploy.sh
@@ -5,11 +5,16 @@ set -o errexit
 
 
 # variables that we use
-REPO_URL="http://smalltalkhub.com/mc/YuriyTymchuk/ScrapYard/main/"
-PACKAGE_NAME="ConfigurationOfFlatQA"
+readonly REPO_URL="http://smalltalkhub.com/mc/YuriyTymchuk/ScrapYard/main/"
+readonly PACKAGE_NAME="ConfigurationOfFlatQA"
+readonly DEPLOYMENT_DIR=$(readlink -m $(dirname $SMALLTALK_CI_IMAGE))
+readonly DEPLOYMENT_IMAGE=$DEPLOYMENT_DIR/Deploy.image
+
+#copy the image
+$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE save Deploy
 
 # load the conf package
-$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
+$SMALLTALK_CI_VM $DEPLOYMENT_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
 
 # store the version
-$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
+$SMALLTALK_CI_VM $DEPLOYMENT_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
