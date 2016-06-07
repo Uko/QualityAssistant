a378b01930a08f22ac688e64c70cb1372d90ca7f
Uko <uko@unikernel.net>
2016-06-07 19:36:15 +0200

    Add author name and hold the door!

--- .utility/flatDeploy.sh
+++ .utility/flatDeploy.sh
@@ -11,8 +11,13 @@ readonly PACKAGE_NAME="ConfigurationOfFlatQA"
 #set the correct vm
 SMALLTALK_CI_VM=`find $SMALLTALK_CI_VMS -name pharo -type f -perm +0111 | head -n 1`
 
+#set the author name or it will get crazy…
+$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval --save "Author fullName: 'JohnSnow'"
+
 # load the conf package
 $SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
 
 # store the version
+echo Creating version $TRAVIS_TAG
 $SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
+echo Done!
