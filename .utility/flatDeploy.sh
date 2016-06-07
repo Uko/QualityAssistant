5ebb6411c47587deb0f2c689646c3efa058159c2
Uko <uko@unikernel.net>
2016-06-07 17:02:00 +0200

    another take on loading vm for deploy

--- .utility/flatDeploy.sh
+++ .utility/flatDeploy.sh
@@ -7,14 +7,11 @@ set -o errexit
 # variables that we use
 readonly REPO_URL="http://smalltalkhub.com/mc/YuriyTymchuk/ScrapYard/main/"
 readonly PACKAGE_NAME="ConfigurationOfFlatQA"
-readonly DEPLOYMENT_DIR=$(readlink -m $(dirname $SMALLTALK_CI_IMAGE))
-readonly DEPLOYMENT_IMAGE=$DEPLOYMENT_DIR/Deploy.image
 
-#copy the image
-$SMALLTALK_CI_VM $SMALLTALK_CI_IMAGE save Deploy
+curl get.pharo.org/vm60 | bash
 
 # load the conf package
-$SMALLTALK_CI_VM $DEPLOYMENT_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
+./pharo $SMALLTALK_CI_IMAGE eval --save "Gofer new url: '$REPO_URL' username: '$HUB_USER' password: '$HUB_PASS'; package: '$PACKAGE_NAME'; load."
 
 # store the version
-$SMALLTALK_CI_VM $DEPLOYMENT_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
+./pharo $SMALLTALK_CI_IMAGE eval "ConfigurationOfFlatQA makeVersion: '$TRAVIS_TAG'"
